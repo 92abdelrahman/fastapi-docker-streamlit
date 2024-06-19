3f63f4e039183ee99f927ccd8326238e19ac8590
Abdelrahman Naser <a.naser.cse@gmail.com>
2024-06-19 05:20:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -37,6 +37,3 @@ async def receive_updates(request: Request):
         print(f'An error occurred: {e}')
     return {"status": "ok"}
 
-if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=7860)
